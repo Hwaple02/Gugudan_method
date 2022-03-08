6ae10a3b14ec26edc2ffb0e74d317a09f2b6eb14
Ben Kang <Ben Kang@DESKTOP-6RPFV0J>
2022-03-08 13:02:26 +0900

    guide message

--- src/gugu_method.java
+++ src/gugu_method.java
@@ -20,7 +20,7 @@ public class gugu_method {
 	public static void main(String[] args) {
 //		Scanner s = new Scanner(System.in);
 //		int n = s.nextInt();
-		
+		System.out.println("구구단을 출력합니다");
 		for(int i=2; i<10; i++)
 		{
 			int result[] = cal(i);
